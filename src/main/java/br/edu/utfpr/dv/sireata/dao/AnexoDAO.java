954fa637c63571eff6b25e0a5f083d3550c616c4
Juliana Yukari <48594685+julianayukari@users.noreply.github.com>
2020-09-29 21:57:53 -0300

    Uso do try-with-resources na classe AnexoDAO

--- src/main/java/br/edu/utfpr/dv/sireata/dao/AnexoDAO.java
+++ src/main/java/br/edu/utfpr/dv/sireata/dao/AnexoDAO.java
@@ -13,45 +13,26 @@ import br.edu.utfpr.dv.sireata.model.Anexo;
 public class AnexoDAO {
 	
 	public Anexo buscarPorId(int id) throws SQLException{
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		
-		try{
-			conn = ConnectionDAO.getInstance().getConnection();
-			stmt = conn.prepareStatement("SELECT anexos.* FROM anexos " +
-				"WHERE idAnexo = ?");
-		
+
+		try(Connection conn = ConnectionDAO.getInstance().getConnection();
+				PreparedStatement stmt = conn.prepareStatement("SELECT anexos.* FROM anexos " + "WHERE idAnexo = ?");){
+			
 			stmt.setInt(1, id);
-			
-			rs = stmt.executeQuery();
-			
-			if(rs.next()){
-				return this.carregarObjeto(rs);
-			}else{
-				return null;
+	
+			try(ResultSet rs = stmt.executeQuery()){
+				if(rs.next()){
+					return this.carregarObjeto(rs);
+				}else{
+					return null;
+				}	
 			}
-		}finally{
-			if((rs != null) && !rs.isClosed())
-				rs.close();
-			if((stmt != null) && !stmt.isClosed())
-				stmt.close();
-			if((conn != null) && !conn.isClosed())
-				conn.close();
 		}
 	}
-	
+		
 	public List<Anexo> listarPorAta(int idAta) throws SQLException{
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		
-		try{
-			conn = ConnectionDAO.getInstance().getConnection();
-			stmt = conn.createStatement();
-		
-			rs = stmt.executeQuery("SELECT anexos.* FROM anexos " +
-				"WHERE idAta=" + String.valueOf(idAta) + " ORDER BY anexos.ordem");
+		try(Connection conn = ConnectionDAO.getInstance().getConnection();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery("SELECT anexos.* FROM anexos " + "WHERE idAta=" + String.valueOf(idAta) + " ORDER BY anexos.ordem");){
 		
 			List<Anexo> list = new ArrayList<Anexo>();
 			
@@ -60,75 +41,60 @@ public class AnexoDAO {
 			}
 			
 			return list;
-		}finally{
-			if((rs != null) && !rs.isClosed())
-				rs.close();
-			if((stmt != null) && !stmt.isClosed())
-				stmt.close();
-			if((conn != null) && !conn.isClosed())
-				conn.close();
 		}
 	}
 	
 	public int salvar(Anexo anexo) throws SQLException{
 		boolean insert = (anexo.getIdAnexo() == 0);
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		
-		try{
-			conn = ConnectionDAO.getInstance().getConnection();
-		
-			if(insert){
-				stmt = conn.prepareStatement("INSERT INTO anexos(idAta, ordem, descricao, arquivo) VALUES(?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
-			}else{
-				stmt = conn.prepareStatement("UPDATE anexos SET idAta=?, ordem=?, descricao=?, arquivo=? WHERE idAnexo=?");
+		if(insert) {
+			return insert(anexo);
+		}
+		else {
+			return update(anexo);
+		}
+	}
+	
+	public int insert(Anexo anexo) throws SQLException{
+	
+		try(Connection conn = ConnectionDAO.getInstance().getConnection();
+				PreparedStatement stmt  = conn.prepareStatement("INSERT INTO anexos(idAta, ordem, descricao, arquivo) VALUES(?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS); ){
+			
+			stmt.setInt(1, anexo.getAta().getIdAta());
+			stmt.setInt(2, anexo.getOrdem());
+			stmt.setString(3, anexo.getDescricao());
+			stmt.setBytes(4, anexo.getArquivo());	
+			stmt.execute();
+			
+			try(ResultSet rs = stmt.getGeneratedKeys();){	
+				if(rs.next()){
+					anexo.setIdAnexo(rs.getInt(1));
+				}
+				return anexo.getIdAnexo();
 			}
+		}
+	}
+	
+	public int update(Anexo anexo) throws SQLException{
+		try(Connection conn = ConnectionDAO.getInstance().getConnection();
+				PreparedStatement stmt = conn.prepareStatement("UPDATE anexos SET idAta=?, ordem=?, descricao=?, arquivo=? WHERE idAnexo=?");){
 			
 			stmt.setInt(1, anexo.getAta().getIdAta());
 			stmt.setInt(2, anexo.getOrdem());
 			stmt.setString(3, anexo.getDescricao());
 			stmt.setBytes(4, anexo.getArquivo());
-			
-			if(!insert){
-				stmt.setInt(5, anexo.getIdAnexo());
-			}
-			
+			stmt.setInt(5, anexo.getIdAnexo());
 			stmt.execute();
 			
-			if(insert){
-				rs = stmt.getGeneratedKeys();
-				
-				if(rs.next()){
-					anexo.setIdAnexo(rs.getInt(1));
-				}
-			}
-			
 			return anexo.getIdAnexo();
-		}finally{
-			if((rs != null) && !rs.isClosed())
-				rs.close();
-			if((stmt != null) && !stmt.isClosed())
-				stmt.close();
-			if((conn != null) && !conn.isClosed())
-				conn.close();
+			
 		}
 	}
 	
 	public void excluir(int id) throws SQLException{
-		Connection conn = null;
-		Statement stmt = null;
-		
-		try{
-			conn = ConnectionDAO.getInstance().getConnection();
-			stmt = conn.createStatement();
-		
+		try (Connection conn = ConnectionDAO.getInstance().getConnection();
+				Statement stmt = conn.createStatement();){
+	
 			stmt.execute("DELETE FROM anexos WHERE idanexo=" + String.valueOf(id));
-		}finally{
-			if((stmt != null) && !stmt.isClosed())
-				stmt.close();
-			if((conn != null) && !conn.isClosed())
-				conn.close();
 		}
 	}
 	
